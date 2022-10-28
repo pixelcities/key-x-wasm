30b3b454706c1ff0a573dcaf6a3669972f53ee67
coinshot <93342644+coinshot@users.noreply.github.com>
2022-10-28 18:32:35 +0200

    Include X-CSRF-Token header where needed

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,34 @@ use wasm_bindgen::prelude::*;
 
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::*;
-use web_sys::{Request, RequestInit, RequestCredentials, RequestMode, Response};
+use web_sys::{Request, RequestInit, RequestCredentials, RequestMode, Response, HtmlDocument};
+
+fn document() -> HtmlDocument {
+    web_sys::window().unwrap().document().unwrap().dyn_into::<HtmlDocument>().unwrap()
+}
+
+pub fn get_cookie(name: &str) -> String {
+    let cookies = document().cookie().unwrap();
+    let value = cookies
+        .split(';')
+        .find_map(|kv|
+            if kv.starts_with(name) {
+                let parts = kv.split("=").collect::<Vec<&str>>();
+                if parts.len() > 1 {
+                    Some(parts[1].to_owned())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        );
+
+    match value {
+        Some(v) => v,
+        None => "".to_owned()
+    }
+}
 
 pub async fn request(method: String, url: String, payload: Option<String>) -> JsValue {
     let mut opts = RequestInit::new();
@@ -20,6 +47,10 @@ pub async fn request(method: String, url: String, payload: Option<String>) -> Js
     let headers = request.headers();
     headers.set("Content-Type", "application/json").unwrap();
     headers.set("Accept", "application/json").unwrap();
+
+    if method != "GET" {
+        headers.set("X-CSRF-Token", get_cookie("_mycelium_csrf_token").as_str()).unwrap();
+    }
 
     let window = web_sys::window().unwrap();
     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.unwrap();
