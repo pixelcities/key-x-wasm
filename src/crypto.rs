d0d1b4cb2b14140d7c9e6d86fe75b93d2b494608
coinshot <93342644+coinshot@users.noreply.github.com>
2023-05-29 21:43:37 +0200

    Enable AES128

--- src/crypto.rs
+++ src/crypto.rs
@@ -5,7 +5,7 @@ use std::str;
 
 use rand::rngs::OsRng;
 use rand::{CryptoRng, Rng};
-use aes_gcm_siv::{Aes256GcmSiv, Key as AesKey, Nonce};
+use aes_gcm_siv::{Aes128GcmSiv, Aes256GcmSiv, Key as AesKey, Nonce};
 use aes_gcm_siv::aead::{Aead, NewAead};
 
 pub fn gen_nonce<T>(csprng: &mut T) -> [u8; 12] where T: CryptoRng + Rng, {
@@ -29,11 +29,18 @@ pub fn gen_key_32<T>(csprng: &mut T) -> [u8; 32] where T: CryptoRng + Rng, {
 pub fn encrypt_custom(plaintext: &String, secret_key: &[u8]) -> String {
     let mut csprng = OsRng;
 
-    let key = AesKey::from_slice(secret_key);
-    let cipher = Aes256GcmSiv::new(key);
     let nonce = gen_nonce(&mut csprng);
 
-    let ciphertext = cipher.encrypt(Nonce::from_slice(&nonce), plaintext.as_bytes()).expect("encryption failure!");
+    let ciphertext = if secret_key.len() == 16 {
+        let key = AesKey::from_slice(secret_key);
+        let cipher = Aes128GcmSiv::new(key);
+        cipher.encrypt(Nonce::from_slice(&nonce), plaintext.as_bytes()).expect("encryption failure!")
+
+    } else {
+        let key = AesKey::from_slice(secret_key);
+        let cipher = Aes256GcmSiv::new(key);
+        cipher.encrypt(Nonce::from_slice(&nonce), plaintext.as_bytes()).expect("encryption failure!")
+    };
 
     format!("{}:{}", base64::encode(nonce), base64::encode(ciphertext))
 }
@@ -43,12 +50,23 @@ pub fn decrypt_custom(ciphertext: &String, secret_key: &[u8]) -> Result<String, 
     let nonce: Vec<u8> = base64::decode(split[0]).unwrap();
     let bytes = base64::decode(split[1]).unwrap();
 
-    let key = AesKey::from_slice(secret_key);
-    let cipher = Aes256GcmSiv::new(key);
+    if secret_key.len() == 16 {
+        let key = AesKey::from_slice(secret_key);
+        let cipher = Aes128GcmSiv::new(key);
 
-    match cipher.decrypt(Nonce::from_slice(&nonce), bytes.as_ref()) {
-        Ok(plaintext) => Ok(str::from_utf8(&plaintext).unwrap().to_string()),
-        Err(_) => Err("decryption failure!".to_string())
+        match cipher.decrypt(Nonce::from_slice(&nonce), bytes.as_ref()) {
+            Ok(plaintext) => Ok(str::from_utf8(&plaintext).unwrap().to_string()),
+            Err(_) => Err("decryption failure!".to_string())
+        }
+
+    } else {
+        let key = AesKey::from_slice(secret_key);
+        let cipher = Aes256GcmSiv::new(key);
+
+        match cipher.decrypt(Nonce::from_slice(&nonce), bytes.as_ref()) {
+            Ok(plaintext) => Ok(str::from_utf8(&plaintext).unwrap().to_string()),
+            Err(_) => Err("decryption failure!".to_string())
+        }
     }
 }
 
